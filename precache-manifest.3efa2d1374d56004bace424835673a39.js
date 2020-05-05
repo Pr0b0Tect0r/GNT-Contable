1fa19540f31730f6f2f9f1d4443ce3e98a60e8bb
Samuel Bustamante <programador@gnt.pe>
2020-05-05 17:54:14 -0500

    Updates

---
+++ precache-manifest.3efa2d1374d56004bace424835673a39.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "271af8453d69577bcf25c5db70f13c94",
+    "url": "/GNT-Contable/index.html"
+  },
+  {
+    "revision": "63271ea91820605fc46b",
+    "url": "/GNT-Contable/static/js/0.fd6f70e5.chunk.js"
+  },
+  {
+    "revision": "8b0766a9fbad5ca9b4ed",
+    "url": "/GNT-Contable/static/js/1.35a8645b.chunk.js"
+  },
+  {
+    "revision": "1c61aaa4e8380010042b",
+    "url": "/GNT-Contable/static/js/10.920b171e.chunk.js"
+  },
+  {
+    "revision": "7ef4b96d838efc7e42cf",
+    "url": "/GNT-Contable/static/js/11.c85e8f71.chunk.js"
+  },
+  {
+    "revision": "6d0546d63b01ee1fd24f",
+    "url": "/GNT-Contable/static/js/12.2fbeb120.chunk.js"
+  },
+  {
+    "revision": "963cb1709e476927829c",
+    "url": "/GNT-Contable/static/js/13.6cad2490.chunk.js"
+  },
+  {
+    "revision": "efee2e7fff8c7c4c6e39",
+    "url": "/GNT-Contable/static/js/2.63fba5b3.chunk.js"
+  },
+  {
+    "revision": "bd49b04565accd5b6f2624c86f8385ba",
+    "url": "/GNT-Contable/static/js/2.63fba5b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f77866bab22833f1b8f6",
+    "url": "/GNT-Contable/static/js/5.c04b6262.chunk.js"
+  },
+  {
+    "revision": "6667dc153edcbe95e8723d6c8f6541e2",
+    "url": "/GNT-Contable/static/js/5.c04b6262.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da720b923ed2c9f71596",
+    "url": "/GNT-Contable/static/js/6.2efce9ae.chunk.js"
+  },
+  {
+    "revision": "aa762385ab423a29206547471fcc4bac",
+    "url": "/GNT-Contable/static/js/6.2efce9ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02a094756c64b6b3c4c0",
+    "url": "/GNT-Contable/static/js/7.26f4af23.chunk.js"
+  },
+  {
+    "revision": "e8c2eb80ff50e89c5861",
+    "url": "/GNT-Contable/static/js/8.a579f74b.chunk.js"
+  },
+  {
+    "revision": "a315e6ce09b8dd8f2803",
+    "url": "/GNT-Contable/static/js/9.1575d68c.chunk.js"
+  },
+  {
+    "revision": "969436cdc524c1bd4d02",
+    "url": "/GNT-Contable/static/js/main.e5408b76.chunk.js"
+  },
+  {
+    "revision": "a410a1683475ae31740c",
+    "url": "/GNT-Contable/static/js/runtime-main.3925c016.js"
+  },
+  {
+    "revision": "38d178b9f893e7d71887930af67bb51e",
+    "url": "/GNT-Contable/static/media/Logo.38d178b9.svg"
+  }
+]);
